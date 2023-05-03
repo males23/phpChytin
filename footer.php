b7098554ee604104c4e2cf4d34a9b4feb09d3e34
Chytin ML <131170034+males23@users.noreply.github.com>
2023-05-03 14:02:50 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,6 @@
-<p>Copyright moi, tous droits réservés</p>
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+  </div>
+</footer>
