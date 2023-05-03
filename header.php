842173ed15ee4483112cccfce92018d8763c00d3
Chytin ML <131170034+males23@users.noreply.github.com>
2023-05-03 13:40:14 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<nav id="menu">        
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+            <li><a href="page1.html">Lien1</a></li>
+            <li><a href="page2.html">Lien2</a></li>
+            <li><a href="page3.html">Lien3</a></li>
+        </ul>
+    </div>    
+</nav>
