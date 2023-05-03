40dbb8262d3b647bce8b89d22535f74fd11610bb
Chytin ML <131170034+males23@users.noreply.github.com>
2023-05-03 19:49:26 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
-<!-- contact.php -->
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de recettes - Formulaire de Contact</title>
+    <title>Site de Recettes - Page d'accueil</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
         rel="stylesheet"
@@ -16,7 +16,7 @@
 
     <?php include_once('header.php'); ?>
         <h1>Contactez nous</h1>
-        <form>
+        <form action="submit_contact.php" method="POST">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -24,7 +24,7 @@
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
-                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="textarea"></textarea>
+                <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
