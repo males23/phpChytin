f06671fa23e5383ae98d4d64c92b06fdd8690ce0
Chytin ML <131170034+males23@users.noreply.github.com>
2023-05-09 16:19:48 +0200

    Add files via upload

---
+++ partage_de_recettes.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le : mar. 09 mai 2023 à 14:16
+-- Version du serveur : 5.7.39
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `partage_de_recettes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `full_name` varchar(128) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `age` int(11) NOT NULL,
+  `adresse_postale` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`user_id`, `full_name`, `email`, `password`, `age`, `adresse_postale`) VALUES
+(1, 'Chytin MALELA', 'aime.malela@yahoo.fr', 'Bonjour', 32, '12 Rue balma, 34080 Montpellier'),
+(2, 'Agnes NDOUR', 'mamitasar@yahoo.fr', 'Chytin23', 34, ''),
+(3, 'Roy MALELA', 'roy-m@gmail.com', 'Devine', 8, ''),
+(4, 'Charly Males', 'charly@yahoo.fr', 'Aussi90', 62, '');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
